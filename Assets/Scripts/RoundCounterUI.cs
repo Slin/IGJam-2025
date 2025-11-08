e1682c2261db89853f49ce80ca018627368288c3
Nils Daumann <nils_daumann@slindev.com>
2025-11-08 18:01:32 +0100

    Round counter ui updates now.

---
+++ Assets/Scripts/RoundCounterUI.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+using TMPro;
+
+[DisallowMultipleComponent]
+public class RoundCounterUI : MonoBehaviour
+{
+	public TMP_Text tmpText;
+	bool _subscribed;
+	Coroutine _waitSubscribe;
+
+	void Awake()
+	{
+		if (tmpText == null) tmpText = GetComponent<TMP_Text>();
+		if (tmpText == null)
+		{
+			var go = GameObject.Find("RoundCounter");
+			if (go != null)
+			{
+				tmpText = go.GetComponent<TMP_Text>();
+			}
+		}
+	}
+
+	void OnEnable()
+	{
+		TrySubscribeAndRefresh();
+		if (!_subscribed)
+		{
+			_waitSubscribe = StartCoroutine(WaitAndSubscribe());
+		}
+	}
+
+	void OnDisable()
+	{
+		var p = PlayerStatsManager.Instance;
+		if (p != null)
+		{
+			p.onRoundChanged.RemoveListener(UpdateText);
+		}
+		_subscribed = false;
+		if (_waitSubscribe != null)
+		{
+			StopCoroutine(_waitSubscribe);
+			_waitSubscribe = null;
+		}
+	}
+
+	void UpdateText(int round)
+	{
+		string text = string.Format("{0}", round);
+		if (tmpText != null) tmpText.text = text;
+		Debug.Log($"RoundCounterUI: Round changed to {round}");
+	}
+
+	void TrySubscribeAndRefresh()
+	{
+		var p = PlayerStatsManager.Instance;
+		if (p == null) return;
+		if (!_subscribed)
+		{
+			p.onRoundChanged.AddListener(UpdateText);
+			_subscribed = true;
+		}
+		UpdateText(p.CurrentRound);
+	}
+
+	System.Collections.IEnumerator WaitAndSubscribe()
+	{
+		while (PlayerStatsManager.Instance == null)
+		{
+			yield return null;
+		}
+		_waitSubscribe = null;
+		TrySubscribeAndRefresh();
+	}
+}
+
+
